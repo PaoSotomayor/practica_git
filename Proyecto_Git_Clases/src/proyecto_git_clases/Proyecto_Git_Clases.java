f5499fda8fd6f419a018d3310b0a647606d4aef8
Pao Sotomayor <126527997+PaoSotomayor@users.noreply.github.com>
2023-07-30 15:41:27 -0500

    Add files via upload

---
+++ Proyecto_Git_Clases/src/proyecto_git_clases/Proyecto_Git_Clases.java
@@ -0,0 +1,15 @@
+
+package proyecto_git_clases;
+
+//Subiendo proyecto vacío.....Vamos a crear las clases.
+
+public class Proyecto_Git_Clases {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
