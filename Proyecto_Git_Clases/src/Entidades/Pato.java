bf7aef35677c9c919a4696ed9e617181934b04a5
Pao Sotomayor <126527997+PaoSotomayor@users.noreply.github.com>
2023-07-30 15:50:40 -0500

    Add files via upload
    Subida la clase Pato en mi rama PaoBranch.

---
+++ Proyecto_Git_Clases/src/Entidades/Pato.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author user
+ */
+public class Pato {
+    private String nombre;
+    private int edad;
+    private int peso;
+    private boolean revisado;
+    
+}
